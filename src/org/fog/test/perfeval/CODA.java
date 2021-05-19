3f79b08b35505bd849eea870285db0fe2b452c09
Narges Mehran <39765584+SiNa88@users.noreply.github.com>
2021-05-19 20:12:02 +0200

    Update and rename VRGameFogMatching3.java to CODA.java

--- src/org/fog/test/perfeval/CODA.java
+++ src/org/fog/test/perfeval/CODA.java
@@ -38,7 +38,7 @@ import org.fog.utils.TimeKeeper;
 import org.fog.utils.distribution.DeterministicDistribution;
 
 
-public class VRGameFogMatching3 {
+public class CODA {
 	static List<FogDevice> fogDevices = new ArrayList<FogDevice>();
 	static List<Sensor> sensors = new ArrayList<Sensor>();
 	static List<Actuator> actuators = new ArrayList<Actuator>();
